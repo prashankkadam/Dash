df18f515fecd3decc9723f6c9a6cf444d31b7d8d
prashank kadam <prashankkadam07@gmail.com>
2019-07-31 14:36:15 +0300

    parallel threaded print program for dashboard

---
+++ print_pdf.py
@@ -0,0 +1,19 @@
+# -*- coding:/ utf-8 -*-
+"""
+Created on Tue Jul 23 12:07:20 2019
+This piece of software is bound by The MIT License (MIT)
+Copyright (c) 2019 Prashank Kadam
+Code written by : Prashank Kadam
+User name - ADM-PKA187
+Email ID : prashank.kadam@maersktankers.com
+Created on - Wed Jul 31 11:46:22 2019
+version : 1.0
+"""  
+
+import pdfkit
+import time
+
+path_wkthmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+config = pdfkit.configuration(wkhtmltopdf=path_wkthmltopdf)
+time.sleep(5)
+pdfkit.from_url('http://127.0.0.1:8050/', r'C:\Users\ADM-PKA187\Desktop\Dastabase\output.pdf',configuration=config, cover_first=True)
